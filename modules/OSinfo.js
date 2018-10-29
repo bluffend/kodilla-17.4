29891eb7dd83426b206170ea876f80c12733e2c3
Slawomir Masluk <slawomirmasluk@gmail.com>
2018-10-29 15:16:59 +0100

    Add file package-lock.json & add changes in file OSinfo.js to add colors to categories

--- modules/OSinfo.js
+++ modules/OSinfo.js
@@ -1,5 +1,6 @@
 var os = require('os');
 var uptimeValue = require('../modules/uptime');
+var colors = require('colors');
 
 function getOSinfo() {
     var type = os.type();
@@ -12,11 +13,11 @@ function getOSinfo() {
     var cpu = os.cpus()[0].model;
     var uptime = os.uptime();
     var userInfo = os.userInfo();
-    console.log('System:', type);
-    console.log('Release:', release);
-    console.log('CPU model:', cpu);
-    console.log('Uptime: ', uptimeValue.getuptime(uptime));
-    console.log('User name:', userInfo.username);
+    console.log('System:'.gray, type);
+    console.log('Release:'.red, release);
+    console.log('CPU model:'.magenta, cpu);
+    console.log('Uptime: '.green, uptimeValue.getuptime(uptime));
+    console.log('User name:'.yellow, userInfo.username);
     console.log('Home dir:', userInfo.homedir);
 }
 
